a78fde98a4df7871c787c971a93f9ebc0e13093d
sanssitk <sanssitk@gmail.com>
2021-02-13 17:42:35 -0600

    Navbar completed

--- src/Routes/HomePage/Home.js
+++ src/Routes/HomePage/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./Home.css";
 import FaceBookSignIn from "../../Provider/FaceBookSignIn";
 import { useStateValue } from "../../StateManagement/stateProvider";
+import NavBar from "./NavBar";
 
 function Home() {
   const [{ user }, dispatch] = useStateValue();
   return (
     <div className="home">
+      <NavBar />
+      <div className="bodyContainer"></div>
       {user ? "Home page " : "Home page Not Login in"}
       <FaceBookSignIn />
     </div>

7bc9bbe17cb9199a18b3a32df18a534722c9cb66
sanssitk <sanssitk@gmail.com>
2021-02-12 19:28:44 -0600

    firebase-facebook signIn Auth added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import "./App.css";
+import { provider, auth } from "./firebase";
 
 function App() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((res) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        let credential = res.credential;
+        // The signed-in user info.
+        let user = res.user;
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        let accessToken = credential.accessToken;
+        console.log(user);
+        console.log(accessToken);
+      })
+      .catch((err) => {
+        // Handle Errors here.
+        var errorCode = err.code;
+        var errorMessage = err.message;
+        // The email of the user's account used.
+        var email = err.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = err.credential;
+      });
+  };
+
   return (
     <div className="app">
-      <button>Facebook Login</button>
+      <button onClick={signIn}>Facebook Login</button>
     </div>
   );
 }
 
 export default App;
+// Also notice that you can retrieve the Facebook provider's OAuth token which can be used to fetch additional data using the Facebook APIs.
---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDzy685cKmdP84s2U5ZEORs0XfLYIVXXNU",
+    authDomain: "shrestha-app.firebaseapp.com",
+    projectId: "shrestha-app",
+    storageBucket: "shrestha-app.appspot.com",
+    messagingSenderId: "1068081297258",
+    appId: "1:1068081297258:web:51073d4fbdfafc8ec05713",
+    measurementId: "G-7LKWWTE3JY"
+  };
+
+    // Initializing firebase with config
+    const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+    // Creating dbsetup
+    const db = firebaseApp.firestore();
+  
+    // Authenticating
+    const auth = firebase.auth();
+  
+    // telling firebase to use fb id to login    
+    const provider = new firebase.auth.FacebookAuthProvider();
+  
+    export {auth, provider};
+    export default db;

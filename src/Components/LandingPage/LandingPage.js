4d82fef75db2a9aeb6c3fb6b5c16be49110073ad
sanjay <sanssitk@gmail.com>
2021-10-07 00:37:53 -0500

    logo animate

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -9,6 +9,12 @@ import { Button, Modal } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { Icon } from "semantic-ui-react";
 import "./LandingPage.css";
+import styled, { keyframes } from "styled-components";
+import { FcConferenceCall } from "react-icons/fc";
+
+{
+  /* <img src="/favicon.ico" alt="logo" /> */
+}
 
 function LandingPage({ newUser }) {
   const history = useHistory();
@@ -18,6 +24,63 @@ function LandingPage({ newUser }) {
   const [userSelection, setUserSelection] = useState(null);
   const [userUID, setUserUID] = useState();
   const [userSelected, setUserSelected] = useState(true);
+
+  const LOGO_SIZE = "200px";
+  const DURATION = "0.7s";
+  const SHADOW_HEIGHT = `${Math.round(48 / 5)}px`;
+  const DROP_HEIGHT = "200px";
+  const OFFSET = "200px";
+
+  const bounce = keyframes`
+  from {
+    transform: translateY(0) scale(1);
+  }
+  to {
+    transform: translateY(${DROP_HEIGHT}) scale(1, 0.7);
+  }
+`;
+
+  const grow = keyframes`
+  from {
+    transform: scale(0.2, 0.5);
+  }
+  to {
+    transform: scale(1.5, 0.8);
+  }
+`;
+
+  const LogoWrapper = styled(FcConferenceCall)`
+    width: ${LOGO_SIZE};
+    height: ${LOGO_SIZE};
+    position: absolute;
+    top: ${OFFSET};
+    left: calc(50% - ${LOGO_SIZE} / 2);
+    animation-name: ${bounce};
+    animation-duration: ${DURATION};
+    animation-direction: alternate;
+    animation-timing-function: cubic-bezier(0.95, 0.05, 0.795, 0.035);
+    animation-iteration-count: infinite;
+  `;
+
+  const Shadow = styled.div`
+    width: ${LOGO_SIZE};
+    height: ${SHADOW_HEIGHT};
+    background: radial-gradient(
+      50% 50%,
+      rgba(150, 150, 150, 1),
+      rgba(150, 150, 150, 0.05)
+    );
+    position: absolute;
+    top: calc(
+      ${OFFSET} + ${DROP_HEIGHT} + ${LOGO_SIZE} - 1.5 * ${SHADOW_HEIGHT}
+    );
+    left: calc(50% - ${LOGO_SIZE} / 2);
+    animation-name: ${grow};
+    animation-duration: ${DURATION};
+    animation-direction: alternate;
+    animation-timing-function: cubic-bezier(0.95, 0.05, 0.795, 0.035);
+    animation-iteration-count: infinite;
+  `;
 
   // useEffect(() => {
   //   if (user) {
@@ -177,8 +240,11 @@ function LandingPage({ newUser }) {
 
   return (
     <div className="landingPage">
-      <img src="/favicon.ico" alt="logo" height="100vw" />
-      <h1 style={{ color: "white", fontSize: "3rem" }}>श्रेष्ठ परिवार </h1>
+      <LogoWrapper />
+      <Shadow />
+      <h1 className="title" style={{ color: "white", fontSize: "3rem" }}>
+        श्रेष्ठ परिवार
+      </h1>
       <FbButton text="Login with Facebook" buttonClicked={logInClicked} />
       {open && allMembers ? renderModal() : ""}
     </div>
